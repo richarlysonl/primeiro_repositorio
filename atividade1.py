342f4fbc0843e4431448aa161a72cf42769e45ef
RIcharlyson Luan Andrade <148484101+richarlysonl@users.noreply.github.com>
2025-03-06 22:00:58 -0300

    Create atividade1.py

---
+++ atividade1.py
@@ -0,0 +1,8 @@
+valor1 = float(input(""))
+valor2 = float(input(""))
+valor3 = float(input(""))
+valor1 = valor1 * 2
+valor2 = valor2 * 3
+valor3 = valor3 * 5
+
+print(f"MEDIA = {(valor1 + valor2 + valor3)/10}")
